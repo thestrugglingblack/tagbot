7f42c9ffe50aa722098008006a7e9fcafe83c84a
Zuri Hunter <zuri@blackcape.io>
2025-08-20 11:11:03 -0400

    fix: deleted site

--- site/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./{app,components,libs,pages,hooks}/**/*.{html,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
